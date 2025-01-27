487d133cb60a6e630c06793344cd91d136353273
deisesalless <deisesalles18@gmail.com>
2025-01-26 21:00:44 -0300

    5.0 anotações HTTP na classe controller

--- src/main/java/br/com/curso/springiniciante/controller/HelloWorldController.java
+++ src/main/java/br/com/curso/springiniciante/controller/HelloWorldController.java
@@ -1,13 +1,12 @@
 package br.com.curso.springiniciante.controller;
 
+import br.com.curso.springiniciante.domain.User;
 import br.com.curso.springiniciante.service.HelloWorldService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello-world")
@@ -19,5 +18,16 @@ public class HelloWorldController {
     @GetMapping
     public ResponseEntity<String> helloworld() {
         return ResponseEntity.status(HttpStatus.OK).body(service.helloWorld("Deise"));
+    }
+
+    // localhost:8080/hello-world/teste?filter=deise@gmail.com -> só dá certo porque padronizei que preciso receber a palavra FILTER
+    @PostMapping("teste/{email}")
+    public ResponseEntity<String> helloworld(@PathVariable("email") @RequestParam(value = "filter", defaultValue = "nenhum") String email ) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.helloWorld(email));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> helloworld(@RequestBody User userBody) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.helloWorld(userBody.getName()));
     }
 }
---
+++ src/main/java/br/com/curso/springiniciante/domain/User.java
@@ -0,0 +1,9 @@
+package br.com.curso.springiniciante.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String email;
+}
